05c8b2016873ac3c5332b455b3325009e2d4fcb3
Алексей Смирнов <chemistmail@gmail.com>
2024-02-09 17:30:49 +0300

    parse request

--- nse.go
+++ nse.go
@@ -2,21 +2,39 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
+	"encoding/json"
 	"gopkg.in/mcuadros/go-syslog.v2"
 	"gopkg.in/mcuadros/go-syslog.v2/format"
-	"encoding/json"
 )
 
 type Log struct {
-	Server string
-	Host string
-	Request string
-	Status int
-	Size int
-	RequestTime float64
+	Server      string  `json:"server"`
+	Host        string  `json:"host"`
+	Request     Request  `json:"request"`
+	Status      int     `json:"status"`
+	Size        int     `json:"body_bytes_sent"`
+	RequestTime float64 `json:"request_time"`
 }
 
+type Request struct {
+	Method string
+	Url string
+	HttpVersion string
+}
+
+func (d *Request) UnmarshalJSON(data []byte) error {
+	parts := strings.Split(string(data), " ")
+	if len(parts) < 3 {
+		return fmt.Errorf("request does not have 3 part")
+	}
+
+	d.Method = parts[0]
+	d.Url = parts[1]
+	d.HttpVersion = parts[2]
+	return nil
+}
 
 func main() {
 	fmt.Println("Start syslog server")
@@ -39,8 +57,9 @@ func main() {
 	server.Wait()
 }
 
-func parser(log format.LogParts ) {
+func parser(log format.LogParts) {
 	var ms Log
 	json.Unmarshal([]byte(log["content"].(string)), &ms)
-	fmt.Printf("request: %s status: %d\n", ms.Request, ms.Status)
+	fmt.Printf("request: %s status: %d\n", ms.Request.Url, ms.Status)
+	//fmt.Println(log["content"].(string))
 }
